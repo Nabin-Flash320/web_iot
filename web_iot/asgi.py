c7a3bf31d0189ef53ed971b3ae06ee0f82b5a273
NDJ-EZLO <nabin.dangi@ezlo.com>
2023-07-21 16:17:58 +0545

    Django using websocket

---
+++ web_iot/asgi.py
@@ -0,0 +1,23 @@
+"""
+ASGI config for web_iot project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web_iot.settings")
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application,
+    "websocket":URLRouter([
+        
+    ])
+})
+
