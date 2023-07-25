23bcca2176a0fa8246211381137bfe31001563c8
NDJ-EZLO <nabin.dangi@ezlo.com>
2023-07-25 12:14:59 +0545

    ALLOWED_HOSTS in django project settings.

--- web_iot/settings.py
+++ web_iot/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-d-416)6bqe^ua6e6sf9fm40=jolb8m3*gbg5o!ntchi#q4ojc3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.66']
 
 
 # Application definition
