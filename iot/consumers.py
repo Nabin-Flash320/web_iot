c5891faad25d54ad69a3fb693f1801df9b1bc149
NDJ-EZLO <nabin.dangi@ezlo.com>
2023-07-28 11:26:08 +0545

    Channel layer configurations to be done.

--- iot/consumers.py
+++ iot/consumers.py
@@ -4,7 +4,7 @@ from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
 class IOTConsumerClass(AsyncJsonWebsocketConsumer):
     async def connect(self):
-        await self.accept()
+        await self.accept() # Configuyre django channels' channel layer to get channel_name attrubute.
     
     async def receive(self, text_data=None, bytes_data=None, **kwargs):
         print(text_data)
