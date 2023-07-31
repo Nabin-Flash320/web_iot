ff3ac20b322702ed7ae850ddae45b33548573922
NDJ-EZLO <nabin.dangi@ezlo.com>
2023-07-31 11:22:11 +0545

    Cross socket communication, URLs for UI and devices.

--- iot/consumers.py
+++ iot/consumers.py
@@ -2,40 +2,53 @@
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 import json
 
+ui_connection_list = list()
+device_connection_list = list()
 
-class IOTConsumerClass(AsyncJsonWebsocketConsumer):
-    ui_connection_list = list()
-    device_connection_list = list()
+class IOTUIConsumerClass(AsyncJsonWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
+        print("UI channel {0} connected.".format(self.channel_name))
+        ui_connection_list.append(self.channel_name)
+
+    async def disconnect(self, code):
+        print("UI channel {0} disconnected.".format(self.channel_name))
+        if self.channel_name in ui_connection_list:
+            ui_connection_list.remove(self.channel_name)
+    
+    async def receive(self, text_data=None, bytes_data=None, **kwargs):
+        print(text_data)
+        for device in device_connection_list:
+            await self.channel_layer.send(device, {
+                'type': 'send_to_device',
+                'data': text_data
+            })
+    
+    async def send_to_ui(self, event):
+        await self.send(text_data=event['data'])
+
+
+class IOTDevicesConsumerClass(AsyncJsonWebsocketConsumer):
     async def connect(self):
         await self.accept() # Configuyre django channels' channel layer to get channel_name attrubute.
-        print("Channel {0} connected.".format(self.channel_name))
+        print("Device channel {0}.".format(self.channel_name))
+        device_connection_list.append(self.channel_name)
     
     async def disconnect(self, code):
-        if self.channel_name in IOTConsumerClass.ui_connection_list:
-            print("UI channel {0} disconnected.".format(self.channel_name))
-            IOTConsumerClass.ui_connection_list.remove(self.channel_name)
-        elif self.channel_name in IOTConsumerClass.device_connection_list:
-            print("Device channel {0} disconnected.".format(self.channel_name))
-            IOTConsumerClass.device_connection_list.remove(self.channel_name)
-        return await super().disconnect(code)
+        print("Device channel {0} disconnected.".format(self.channel_name))
+        if self.channel_name in device_connection_list:
+            device_connection_list.remove(self.channel_name)
     
     async def receive(self, text_data=None, bytes_data=None, **kwargs):
         if text_data == "PING":
             await self.send("PONG")
         else:
-            payload_json = json.loads(text_data)
-            print(payload_json)
-            if 'from' in payload_json:
-                print(payload_json['from'])
-                if payload_json['from'] == 'UI':
-                    IOTConsumerClass.ui_connection_list.append(self.channel_name)
-                elif payload_json['from'] == 'device':
-                    IOTConsumerClass.device_connection_list.append(self.channel_name)
-            else:
-                for connection in IOTConsumerClass.ui_connection_list:
-                    print(connection)
-                
+            for ui in ui_connection_list:
+                await self.channel_layer.send(ui, {
+                    'type': 'send_to_ui',
+                    'data': text_data
+                })
 
-    
-    async def broadcast_to_connections(self, event):
-        pass
+    async def send_to_device(self, event):
+        await self.send(text_data=event['data'])
+
--- web_iot/asgi.py
+++ web_iot/asgi.py
@@ -12,14 +12,15 @@ import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from django.urls import path
-from iot.consumers import IOTConsumerClass
+from iot.consumers import IOTUIConsumerClass, IOTDevicesConsumerClass
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web_iot.settings")
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": URLRouter([
-        path("iot", IOTConsumerClass.as_asgi())
+        path("iot/ui", IOTUIConsumerClass.as_asgi()),
+        path("iot/device", IOTDevicesConsumerClass.as_asgi())
     ])
 })
 
